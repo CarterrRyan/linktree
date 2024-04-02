53ff9ca45156d1d214d2cba1a347bb8f70a9beb4
Carter Todd <cartertwork@gmail.com>
2024-04-03 00:05:34 +0100

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
+app.use(cors());
 
 app.get('/api/github',(req,res)=>{
     const githubUrl ='https://github.com/CarterrRyan';
